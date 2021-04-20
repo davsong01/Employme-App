9c0a4e9a9c7703708f35bb03c86b5291d92db2a1
kennietimmie <kehinde.bridgewateracademy@gmail.com>
2021-04-20 08:52:32 +0100

    primary, secondary column and validation rule

--- app/Http/Controllers/SettingsController.php
+++ app/Http/Controllers/SettingsController.php
@@ -29,16 +29,16 @@ class SettingsController extends Controller
         //
     }
 
-   
+
     public function store(Request $request)
     {
         //
     }
 
-  
+
     public function show(Settings $setting)
     {
-        
+
     }
 
 
@@ -48,31 +48,39 @@ class SettingsController extends Controller
         return view('dashboard.admin.settings.edit', compact('setting'));
     }
 
-  
+
     public function update(Request $request, Settings $setting)
     {
-        // dd($request->all());
+        $this->validate($request, [
+            'OFFICIAL_EMAIL' => 'required|email|unique:users,email,' . $setting->id,
+            'ADDRESS_ON_RECEIPT' => 'required',
+            'CURR_ABBREVIATION' => 'required',
+            'DEFAULT_CURRENCY' => 'required',
+            'primary_color' => 'required|regex:/^#[\da-f]{6}/i',
+            'secondary_color' => 'required|regex:/^#[\da-f]{6}/i',
+        ]);
+
 
             $imgName = $request->logo->getClientOriginalName();
-            // 
+            //
             $logo = (string) Image::make($request->logo)->resize(152, 60)->save('assets/images'.'/'.$imgName, 80, 'png');
-           
- 
+
+
 // $jpg = (string) Image::make('public/'.$file)->encode('png', 90);
         // $image = Image::make($request->logo)->resize(152, 60);
- 
-       
+
+
         $setting->update([
             'OFFICIAL_EMAIL' => $request->OFFICIAL_EMAIL,
             'ADDRESS_ON_RECEIPT' => $request->ADDRESS_ON_RECEIPT,
             'CURR_ABBREVIATION' => $request->CURR_ABBREVIATION,
             'DEFAULT_CURRENCY' => $request->DEFAULT_CURRENCY,
         ]);
-        
+
         return back()->with('message', 'Update successful');
     }
 
- 
+
     public function destroy(Settings $settings)
     {
         //
--- resources/views/dashboard/admin/settings/edit.blade.php
+++ resources/views/dashboard/admin/settings/edit.blade.php
@@ -9,7 +9,7 @@
                     <div class="card-title">
                         @include('layouts.partials.alerts')
                         <h4 class="card-title">Update App Settings</h4>
-                    </div> 
+                    </div>
                    <form action="{{route('settings.update', 1)}}" method="POST"
                         enctype="multipart/form-data" class="pb-2">
                         {{ method_field('PATCH') }}
@@ -19,7 +19,7 @@
                             <label>Official Email *</label>
                             <input type="text" name="OFFICIAL_EMAIL" value="{{ old('OFFICIAL_EMAIL') ?? $setting->OFFICIAL_EMAIL}}" class="form-control" required>
                         </div>
-                        
+
                         <!--Gives the first error for input name-->
                         <div><small style="color:red">{{ $errors->first('OFFICIAL_EMAIL')}}</small></div>
 
@@ -32,17 +32,29 @@
 
                         <div class="form-group">
                             <label>Currency Abbreviation *</label>
-                            <input type="text" name="CURR_ABBREVIATION" value="{{ old('CURR_ABBREVIATION') ??  $setting->CURR_ABBREVIATION}}"value="{{ old('CURR_ABBREVIATION') ?? $setting->CURR_ABBREVIATION}}" 
+                            <input type="text" name="CURR_ABBREVIATION" value="{{ old('CURR_ABBREVIATION') ??  $setting->CURR_ABBREVIATION}}"value="{{ old('CURR_ABBREVIATION') ?? $setting->CURR_ABBREVIATION}}"
                                 class="form-control" required>
                         </div>
                         <div><small style="color:red">{{ $errors->first('CURR_ABBREVIATION')}}</small></div>
                         <div class="form-group">
                             <label>Default Currency code(Change this to dropdown of naira ghana cedes and dollar) *</label>
-                            <input type="text" name="DEFAULT_CURRENCY" value="{!! old('DEFAULT_CURRENCY') ??  $setting->DEFAULT_CURRENCY !!}"value="{!! old('DEFAULT_CURRENCY') ?? $setting->DEFAULT_CURRENCY !!}" 
+                            <input type="text" name="DEFAULT_CURRENCY" value="{!! old('DEFAULT_CURRENCY') ??  $setting->DEFAULT_CURRENCY !!}"value="{!! old('DEFAULT_CURRENCY') ?? $setting->DEFAULT_CURRENCY !!}"
                                 class="form-control" required>
                         </div>
-                        <div><small style="color:red">{{ $errors->first('DEFAULT_CURRENCY')}}</small></div>             
-                    
+                        <div><small style="color:red">{{ $errors->first('DEFAULT_CURRENCY')}}</small></div>
+
+                        <div class="form-row">
+                            <div class="form-group col-md-6">
+                                <label for="primary_color">Primary colour</label>
+                                <input type="text" class="form-control" id="primary_color" minlength="7" maxlength="7" name="primary_color" placeholder="Eg #000000" title="accepts only color code" pattern="#[\da-f]{6}" value="{{ old('primary_color') ?? $setting->primary_color }}" required>
+                                <div><small style="color:red">{{ $errors->first('primary_color')}}</small></div>
+                            </div>
+                            <div class="form-group col-md-6">
+                                 <label for="secondary_color">Secondary colour</label>
+                                <input type="text" class="form-control" id="secondary_color" minlength="7" maxlength="7" name="secondary_color" placeholder="Eg #000000" title="accepts only color code" pattern="#[\da-f]{6}" value="{{ old('secondary_color') ?? $setting->secondary_color }}" required>
+                                <div><small style="color:red">{{ $errors->first('secondary_color')}}</small></div>
+                            </div>
+                        </div>
                         </div>
                         <div class="col-md-12 col-lg-12 col-sm-12">
                             <div class="form-group">
@@ -50,12 +62,12 @@
                                 <input type="file" name="logo"  class="form-control">
                             </div>
                             <div><small style="color:red">{{ $errors->first('logo')}}</small></div>
-                             
+
                         </div>
                         <div class="col-12">
                              <input type="submit" class="btn btn-primary" style="width:100%">
                         </div>
-                        
+
                     </form>
             </div>
         </div>
