e594e1484ddc39ff16feece88bdfc40881bc5076
davsong16 <david@consode.com>
2023-08-05 19:34:17 +0100

    certificate pages

--- app/Certificate.php
+++ app/Certificate.php
@@ -3,8 +3,9 @@
 namespace App;
 
 use App\User;
-use App\Result;
+use App\Module;
 
+use App\Result;
 use App\Program;
 use Illuminate\Database\Eloquent\Model;
 
@@ -34,14 +35,41 @@ class Certificate extends Model
 
     public function scores(){
         $results = Result::where('user_id', $this->user_id)->where('program_id', $this->program_id)->get();
-        
+        $score_settings = ScoreSetting::whereProgramId($this->program_id)->first();
+
         $data['total'] = 0;
         $data['certification_test_score'] = $results->sum('certification_test_score') ?? 0;
-        $data['class_test_score'] = $results->sum('class_test_score') ?? 0;
         $data['role_play_score'] = $results->sum('role_play_score') ?? 0;
         $data['email_test_score'] = $results->sum('email_test_score') ?? 0;
+        $program_ct_score_settings = $score_settings->class_test ?? null;
+        $passmark = $score_settings->passmark ?? null;
+        $class_test_score = 0;
+        $obtainable  = 0;
+        foreach($results as $result){
+            if ($result->module->type == 'Class Test') {
+                $u =  Module::where('type', 0)->where('program_id', $this->program_id)->get();
+                $ob = [];
 
-        $data['total'] = $results->sum('certification_test_score') + $results->sum('class_test_score') + $results->sum('role_play_score') + $results->sum('email_test_score');
+                foreach ($u as $t) {
+                    array_push($ob, $t->questions->count());
+                }
+
+                $obtainable = array_sum($ob);
+               
+                if ($u->count() > 0) {
+                    $class_test_score = $result->class_test_score + $class_test_score;
+                }
+
+                
+            }  
+
+            
+        }
+        // dd($obtainable);
+        $final_ct_score  = $obtainable > 0 ? round(($class_test_score * $program_ct_score_settings) / $obtainable, 0) : 0;
+         
+        $data['class_test_score'] = $final_ct_score;
+        $data['total'] = $results->sum('certification_test_score') +  $final_ct_score + $results->sum('role_play_score') + $results->sum('email_test_score');
        
         return $data;
       
