9af5d0ef864197daf0800785326317b75f6e47e2
davsong16 <david@consode.com>
2023-08-08 09:13:07 +0100

    select training on certificates page

--- app/Http/Controllers/CertificateController.php
+++ app/Http/Controllers/CertificateController.php
@@ -22,7 +22,7 @@ class CertificateController extends Controller
             // $programs = Program::whereHas('certificates', function ($query) {
             //     return $query;
             // })->withCount('certificates')->orderby('created_at', 'DESC')->get();
-            $programs = Program::withCount('certificates')->orderby('created_at', 'DESC')->get();
+            $programs = Program::withCount('certificates')->where('id', '<>', 1)->whereNULL('parent_id')->orderBy('created_at', 'desc')->get();
             return view('dashboard.admin.certificates.selecttraining', compact('programs', 'i'));
 
         }
