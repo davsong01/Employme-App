e8eef40fb604648407ffa17dd1ffd0dad07de41c
davsong16 <david@consode.com>
2023-10-12 09:00:57 +0100

    added modules to permissions

--- storage/framework/views/770bfd3ee939a2260bad1e27a19422e5f3ca0e35.php
+++
@@ -1,7 +0,0 @@
-<footer class="footer text-center">
-</div>
-        <div class="col-12 col-lg-12">
-        <p style="text-align:center; color:black">©<?php echo e(date("Y")); ?> All Rights Reserved by <?php echo e(config('app.name')); ?> | <a
-                href="https://techdaves.com">Designed by TechDaves</a></p>
-        </div>
-</footer><?php /**PATH /Applications/MAMP/htdocs/employme/resources/views/dashboard/layouts/footer.blade.php ENDPATH**/ ?>
