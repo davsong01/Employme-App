bc5e5b0bbce98428debf3f8ba31c40ee7581b6c2
davsong01 <davsong16@gmail.com>
2023-11-22 20:51:04 +0100

    fixed some changes

--- resources/views/dashboard/admin/results/index.blade.php
+++ resources/views/dashboard/admin/results/index.blade.php
@@ -51,6 +51,7 @@
                                 Not Uploaded
                                 @endif 
                             </td>
+                            
                             <td>
                                 @if(!empty(array_intersect(adminRoles(), auth()->user()->role())) || !empty(array_intersect(graderRoles(), Auth::user()->role())))
                                     @if(isset($score_settings->certification) && $score_settings->certification > 0)
@@ -82,47 +83,45 @@
                                  <strong class="tit" style="color:{{ $total < $user->passmark ? 'red' : 'green'}}">{{ $total }}%</strong> 
                             </td>
                             @endif
-                            {{-- @if(!empty(array_intersect(adminRoles(), auth()->user()->role())) || !empty(array_intersect(graderRoles(), Auth::user()->role())))<br>{{ isset($user->total_cert_score ) ? $user->total_cert_score : '' }}%@endif
-                            @if(!empty(array_intersect(adminRoles(), auth()->user()->role())))<td>{{ $user->final_ct_score }}%</td>@endif
-                             @if(!empty(array_intersect(adminRoles(), auth()->user()->role())) || !empty(array_intersect(facilitatorRoles(), auth()->user()->role())))<td>{{ $user->total_role_play_score }}%</td>@endif
-                            @if(!empty(array_intersect(adminRoles(), auth()->user()->role())) || !empty(array_intersect(graderRoles(), Auth::user()->role())))<td>{{ $user->total_email_test_score }}%</td>@endif
-                            <td>{{ $user->passmark }}%</td>
-                            @if(!empty(array_intersect(adminRoles(), auth()->user()->role())))<td>{{ $user->total_cert_score  + $user->final_ct_score + $user->total_role_play_score + $user->total_email_test_score }}%</td>@endif
-                            @if(!empty(array_intersect(adminRoles(), auth()->user()->role())) || !empty(array_intersect(facilitatorRoles(), auth()->user()->role())))<th>{{ $user->marked_by }}</th>@endif
-                            @if(!empty(array_intersect(adminRoles(), auth()->user()->role())) || !empty(array_intersect(graderRoles(), Auth::user()->role())))<th>{{ $user->grader }}</th>@endif --}}
-                            {{-- <th> {{ $user->cl_module_count}}</th> --}}
+                            
                             
                             <td>
                                 @if( isset($user->result_id)) 
-                                    <div class="btn-group">
-                                        @if($user->redotest == 0 && !empty($user->certification_test_details))
+                                    @if($user->redotest == 0)
+                                        @if (!empty($user->certification_test_details))
                                             <a class="btn btn-info btn-sm" href="{{ route('results.add', ['uid' => $user->user_id, 'pid'=>$user->program_id]) }}"><i
-                                                    class="fa fa-eye"> View/Update </i>
-                                            </a>
-                                            @if(!empty(array_intersect(adminRoles(), auth()->user()->role())) || !empty(array_intersect(graderRoles(), Auth::user()->role())))
-                                            <form onsubmit="return confirm('This will delete this user certification test details and enable test to be re-taken. Are you sure you want to do this?');" action="{{ route('results.destroy', $user->result_id, ['uid' => $user->user_id, 'result' => $user->result_id ]) }}" method="POST">
-                                                {{ csrf_field() }}
-                                                {{method_field('DELETE')}}
-                                                <input type="hidden" name="uid" value="{{ $user->user_id }}">
-                                                <input type="hidden" name="rid" value="{{ $user->result_id }}">
-                                                <input type="hidden" name="pid" value="{{ $user->program_id }}">
-                                                <button type="submit" class="btn btn-danger btn-xsm"> <i
-                                                        class="fa fa-redo"> Enable Resit</i>
-                                                </button>
-                                            </form>
+                                                        class="fa fa-eye"> View/Update </i>
+                                                </a>
+                                                @if(!empty(array_intersect(adminRoles(), auth()->user()->role())) || !empty(array_intersect(graderRoles(), Auth::user()->role())))
+                                                <form onsubmit="return confirm('This will delete this user certification test details and enable test to be re-taken. Are you sure you want to do this?');" action="{{ route('results.destroy', $user->result_id, ['uid' => $user->user_id, 'result' => $user->result_id ]) }}" method="POST">
+                                                    {{ csrf_field() }}
+                                                    {{method_field('DELETE')}}
+                                                    <input type="hidden" name="uid" value="{{ $user->user_id }}">
+                                                    <input type="hidden" name="rid" value="{{ $user->result_id }}">
+                                                    <input type="hidden" name="pid" value="{{ $user->program_id }}">
+                                                    <button type="submit" class="btn btn-danger btn-sm"> <i
+                                                            class="fa fa-redo"> Enable Resit</i>
+                                                    </button>
+                                                </form>
                                             @endif
-                                            @endif
-                                            @if($user->redotest != 0)
-                                            <a onclick="return confirm('This will stop this this user from access to take retest certification test/ Are you sure you want to do this?');" data-toggle="tooltip" data-placement="top" title="Stop user from retaking certification test"
-                                                class="btn btn-warning" href="{{ route('stopredotest',['user_id'=>$user->user_id, 'result_id'=>$user->result_id]) }}"><i
+                                        @endif
+                                    @else
+                                        <a onclick="return confirm('This will stop this this user from access to take retest certification test/ Are you sure you want to do this?');" data-toggle="tooltip" data-placement="top" title="Stop user from retaking certification test"
+                                                class="btn btn-info" href="{{ route('stopredotest',['user_id'=>$user->user_id, 'result_id'=>$user->result_id]) }}"><i
                                                     class="fa fa-stop"></i>
-                                            </a>
-                                            @endif
-                                    
-                                    </div>
+                                        </a>
+                                    @endif
                                 @else
-                                    N/A
+                                 <div class="btn-group">
+                                   <button class="btn btn-button btn-danger" disabled>Participant has not taken any test!</button>
+                                 </div>
                                 @endif
+                                @if(!empty(array_intersect(adminRoles(), auth()->user()->role())))
+                                <a data-toggle="tooltip" data-placement="top" title="Impersonate User"
+                                    class="btn btn-warning" href="{{ route('impersonate', $user->user_id) }}"><i
+                                        class="fa fa-unlock"></i>
+                                </a>
+                                @endif 
                             </td>
                            
                         @endif
