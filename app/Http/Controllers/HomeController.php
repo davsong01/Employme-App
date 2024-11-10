afc0b67de1c6d5e671f2c32e2f67a5e9a2d50385
davsong01 <davsong16@gmail.com>
2024-11-10 15:53:35 +0100

    Updates

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -143,7 +143,7 @@ class HomeController extends Controller
         $program = Program::whereId($request->p_id)->first();
 
         $payment_mode =  PaymentMode::where('id', $data->payment_mode)->first();
-        
+        // dd($payment_mode, $data->payment_mode, $request->all());
         return view('dashboard.student.balance_checkout', compact('data', 'payment_mode', 'program'));
     }
 
