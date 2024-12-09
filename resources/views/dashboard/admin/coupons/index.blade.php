3442ecdc5b1bfc74c099b5f8a622f2648031d73d
davsong01 <davsong16@gmail.com>
2024-12-09 22:52:51 +0100

    moving to program specific permissions

--- resources/views/dashboard/admin/coupons/index.blade.php
+++ resources/views/dashboard/admin/coupons/index.blade.php
@@ -1,3 +1,14 @@
+@php
+    $check = [
+        'coupon.create',
+        'coupon.edit',
+        'coupon.destroy',
+        'coupon.show'
+    ];
+
+    $permissions = canUserAccessPermission($check);
+@endphp
+
 @extends('dashboard.admin.index')
 @section('title', 'All Coupons')
 @section('content')
@@ -6,12 +17,14 @@
     <div class="card">
         <div class="card-title">
             @include('layouts.partials.alerts')
-         </div>
+        </div>
+        @if($permissions['coupon.create'])
         <div class="card-header">
             <div>
                 <h5 class="card-title"> All Coupons <a href="{{route('coupon.create')}}"><button type="button" class="btn btn-outline-primary">Add New Coupon</button></a></h5> 
             </div>
         </div>
+        @endif
             <div class="">
                 <table id="zero_config" class="table table-striped table-bordered">
                     <thead>
@@ -36,9 +49,12 @@
                             <td>{{ $coupon->coupon_users->count() }}</td>                          
                             <td>
                                 <div class="btn-group">
+                                    @if($permissions['coupon.edit'])
                                     <a data-toggle="tooltip" data-placement="top" title="edit coupon details"
                                         class="btn btn-info" href="{{ route('coupon.edit', $coupon->id)}}"><i class="fa fa-edit"></i>
                                     </a>
+                                    @endif
+                                    @if($permissions['coupon.destroy'])
                                         <form action="{{ route('coupon.destroy', $coupon->id) }}" method="POST" onsubmit="return confirm('Are you really sure?');">
                                             {{ csrf_field() }}
                                             {{method_field('DELETE')}}
@@ -48,9 +64,12 @@
                                                     class="fa fa-trash"></i>
                                             </button>
                                         </form>
+                                    @endif
+                                    @if($permissions['coupon.show'])
                                     <a data-toggle="tooltip" data-placement="top" title="View coupon usage"
                                         class="btn btn-primary" href="{{ route('coupon.show', $coupon->id)}}"><i class="fa fa-eye"></i>
                                     </a>
+                                    @endif
                                 </div>
                         </tr>
                         @endforeach
