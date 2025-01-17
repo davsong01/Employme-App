b7dec4c42e132cb20cb44276cbc8d9fc520d4823
davsong01 <davsong16@gmail.com>
2025-01-17 17:15:08 +0100

    Updates

--- app/Models/PaymentThread.php
+++ app/Models/PaymentThread.php
@@ -12,7 +12,7 @@ class PaymentThread extends Model
     protected $guarded = [];
 
     public function admin(){
-        return $this->belongsTo(User::class, 'admin_id');
+        return $this->belongsTo(Admin::class, 'admin_id');
     }
 
     public function user()
