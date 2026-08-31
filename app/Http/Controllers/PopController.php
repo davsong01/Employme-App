c0d59b8e7c835f1dde57d8a1fff375cbd9043e27
davsong01 <davsong16@gmail.com>
2026-08-31 19:24:08 +0100

    Updates

--- app/Http/Controllers/PopController.php
+++ app/Http/Controllers/PopController.php
@@ -663,8 +663,35 @@ class PopController extends Controller
     public function tempDestroy($id)
     {
         $trans = TempTransaction::find($id);
-        $trans->delete();
+        if ($trans) {
+            $trans->delete();
+        }
         return back()->with('message', 'Delete successful');
+    }
+
+    public function bulkDestroy(Request $request)
+    {
+        if (!checkRoleHas(['Admin', 'Facilitator', 'Grader'])) {
+            return back()->with('error', 'You are not authorized to perform this action');
+        }
+
+        $data = $request->validate([
+            'selected_ids' => 'required|array|min:1',
+            'selected_ids.*' => 'integer|exists:temp_transactions,id',
+        ]);
+
+        $deleted = 0;
+
+        foreach ($data['selected_ids'] as $id) {
+            $transaction = TempTransaction::find($id);
+
+            if ($transaction) {
+                $transaction->delete();
+                $deleted++;
+            }
+        }
+
+        return back()->with('message', $deleted . ' record(s) deleted successfully');
     }
 
     public function reconcile()
